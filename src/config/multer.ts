432ac0b2d395eb7d74019dce881d683ba33c7c13
Danielzxccc <araojodaniel321@gmail.com>
2023-10-13 14:12:06 +0800

    test upload storage

---
+++ src/config/multer.ts
@@ -0,0 +1,26 @@
+import multer from 'multer'
+import path from 'path'
+import * as dotenv from 'dotenv'
+dotenv.config()
+
+const imageFolderPath = path.join(__dirname, '../../', 'uploads')
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(
+        null,
+        process.env.NODE_ENV === 'development'
+          ? imageFolderPath
+          : process.env.STORAGE_URL
+      )
+    },
+    filename: function (req, file, cb) {
+      cb(null, Math.random().toFixed(2) + path.extname(file.originalname))
+    },
+  }),
+})
+
+console.log(imageFolderPath)
+
+export default upload
--- src/index.ts
+++ src/index.ts
@@ -5,10 +5,11 @@ import { startSocket } from './controllers/SocketController'
 import session from 'express-session'
 import * as dotenv from 'dotenv'
 import { sessionConfig } from './config/config'
+import path from 'path'
 
 // routers
 import { AuthRouter } from './routers/AuthRouter'
-import { SessionRequest } from 'AuthType'
+import upload from './config/multer'
 
 dotenv.config()
 const app: Express = express()
@@ -19,9 +20,22 @@ app.use(session(sessionConfig))
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.get('/test', (req: SessionRequest, res) => {
-  if (!req.session.user) return res.send('no no no')
-  res.send(req.session.user)
+app.post('/upload', upload.single('image'), (req, res) => {
+  res.json({ data: req.file })
+})
+app.get('/images/:filename', (req, res) => {
+  const imageFolderPath = path.join(__dirname, '../', 'uploads')
+  const { filename } = req.params
+  const imagePath = path.join(
+    process.env.NODE_ENV === 'development'
+      ? imageFolderPath
+      : process.env.STORAGE_URL,
+    filename
+  )
+
+  if (!imagePath) res.status(404).json({ message: "Can't find image" })
+  // Send the image as a response
+  res.sendFile(imagePath)
 })
 
 app.use('/v1/api/auth', AuthRouter)
