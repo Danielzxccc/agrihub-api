d20850c1a879c9e633971bbc0b3a1b2d9328e452
Danielzxccc <araojodaniel321@gmail.com>
2023-10-17 20:48:56 +0800

    add get current user router

--- src/routers/AuthRouter.ts
+++ src/routers/AuthRouter.ts
@@ -5,3 +5,4 @@ export const AuthRouter = express.Router()
 
 AuthRouter.post('/login', AuthController.authenticateUser)
 AuthRouter.post('/register', AuthController.registerUser)
+AuthRouter.get('/me', AuthController.getCurrentUser)
