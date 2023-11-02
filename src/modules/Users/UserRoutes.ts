c478cac555ffc972853b9118c922849cd3972115
Danielzxccc <araojodaniel321@gmail.com>
2023-11-02 15:10:50 +0800

    User Update Profile

--- src/modules/Users/UserRoutes.ts
+++ src/modules/Users/UserRoutes.ts
@@ -1,6 +1,14 @@
+import upload from '../../config/multer'
+import { UserGuard } from '../AuthGuard/UserGuard'
 import * as UserController from './UserController'
 import express from 'express'
 
 export const UserRouter = express.Router()
 
 UserRouter.get('/profile/:username', UserController.findUserProfile)
+UserRouter.put(
+  '/profile/:id',
+  UserGuard(['user']),
+  upload.single('avatar'),
+  UserController.updateUserProfile
+)
