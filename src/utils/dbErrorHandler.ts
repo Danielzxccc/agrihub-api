4fcdd455508fb2c188b797522dbc76d0b094b354
Danielzxccc <103251753+Danielzxccc@users.noreply.github.com>
2023-11-11 21:34:44 +0800

    Daniel (#45)
    * User Update Profile
    
    * add schema definition for questions
    
    * add error logger
    
    * error logger

--- src/utils/dbErrorHandler.ts
+++ src/utils/dbErrorHandler.ts
@@ -1,6 +1,7 @@
 import HttpError from './HttpError'
 
 function dbErrorHandler(error: any) {
+  console.log(error.stack, 'database error')
   if (error instanceof HttpError) {
     throw new HttpError(error.message, error.httpCode || 500)
   }
