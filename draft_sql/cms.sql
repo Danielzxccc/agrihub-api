500f197422494c87e608af6d0a3b975403f9c5fd
Xaalf104 <alsharief.dinglasa06@gmail.com>
2023-12-20 22:41:12 +0800

    About cms update

---
+++ draft_sql/cms.sql
@@ -0,0 +1,24 @@
+CREATE TABLE about_cms(
+    id SERIAL PRIMARY KEY
+    bg_image TEXT,
+    banner TEXT,
+    partnership_img TEXT,
+    qc_image TEXT,
+    president_image TEXT,
+    org_chart TEXT,
+    description TEXT, -- Paragraphs
+    mission TEXT,
+    vision TEXT,
+    partnership TEXT,
+    commitment TEXT,
+    president_message TEXT
+)
+
+CREATE TABLE about_gallery(
+    id SERIAL PRIMARY KEY
+    imagesrc TEXT,
+    createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
+    updatedAt timestamp DEFAULT CURRENT_TIMESTAMP,
+    cms_id INT,
+    FOREIGN KEY (cms_id) REFERENCES about_cms(id) ON DELETE CASCADE
+)
---
+++ src/schema/AboutSchema.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod'
+
+export const UpdateAbout = z.object({
+  body: z.object({
+    bg_image: z.string().optional(),
+    banner: z.string().optional(),
+    partnership_img: z.string().optional(),
+    qc_image: z.string().optional(),
+    president_image: z.string().optional(),
+    org_chart: z.string().optional(),
+    description: z.string().optional(),
+    mission: z.string().optional(),
+    vision: z.string().optional(),
+    partnership: z.string().optional(),
+    commitment: z.string().optional(),
+    president_message: z.string().optional(),
+  }),
+})
