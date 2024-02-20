b89b9e66c1b6971ea33076b73ae63756a6e600ad
Danielzxccc <araojodaniel321@gmail.com>
2024-02-21 01:53:44 +0800

    fix: event banner url source

--- src/modules/Events/EventsService.ts
+++ src/modules/Events/EventsService.ts
@@ -197,9 +197,9 @@ export async function findUnpublishedEvent(id: string) {
 export async function findPublishedEvent(id: string) {
   return await db
     .selectFrom('events as e')
-    .select(({ eb }) => [
+    .select(({ eb, fn, val }) => [
       'e.id',
-      'e.banner',
+      fn<string>('concat', [val(returnObjectUrl()), 'e.banner']).as('banner'),
       'e.event_start',
       'e.event_end',
       'e.location',
