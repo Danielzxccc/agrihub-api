e9b477263a43afceecfd5b90d9a7b65ce47d5364
Danielzxccc <araojodaniel321@gmail.com>
2024-02-21 15:25:29 +0800

    fix: create tag on blogs

--- src/modules/Blogs/BlogsOpenApiSpec.ts
+++ src/modules/Blogs/BlogsOpenApiSpec.ts
@@ -388,6 +388,12 @@
  *         required: true
  *         schema:
  *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: "#/components/schemas/CreateEventTagsRequest"
  *     responses:
  *       "200":
  *         description: Tags added successfully
