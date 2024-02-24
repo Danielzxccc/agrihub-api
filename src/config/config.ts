27faad6bdd38101b677c5a51b2c441012c9a92e9
Danielzxccc <araojodaniel321@gmail.com>
2024-02-24 22:37:09 +0800

    test netlify 2

--- src/config/config.ts
+++ src/config/config.ts
@@ -38,7 +38,7 @@ export const allowedOrigins = [
   'https://qc-agrihub.xyz',
   'https://agrihub.vercel.app',
   'https://agrihub-6vw7t76z8-wolfcarves.vercel.app',
-  'https://65d9fd8fb791cdfbafda3137--subtle-toffee-a853e7.netlify.app',
+  'https://65d9fede4e315f028eaf14bb--subtle-toffee-a853e7.netlify.app',
 ]
 export const corsOptions = {
   origin: (origin: any, callback: any) => {
