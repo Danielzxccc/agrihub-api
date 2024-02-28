03bcc361fae61e46a653c4ec1c939efdb8db0df8
Danielzxccc <araojodaniel321@gmail.com>
2024-02-28 20:48:57 +0800

    try cookie settings v1

--- src/config/config.ts
+++ src/config/config.ts
@@ -25,7 +25,7 @@ export const sessionConfig: SessionOptions = {
     path: '/',
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'lax',
+    sameSite: 'none',
     secure: process.env.NODE_ENV === 'production',
   },
 }
