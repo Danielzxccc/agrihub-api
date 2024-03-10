5b1e9f0ef5a5b017785f68363afc46dcae0732c3
Danielzxccc <araojodaniel321@gmail.com>
2024-03-10 12:04:53 +0800

    trust proxy v3

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app: Express = express()
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.set('trust proxy', 1)
+// app.set('trust proxy', 1)
 
 app.use(session(sessionConfig))
 
@@ -27,6 +27,7 @@ const io = new Server(httpServer, { cors: corsOptions })
 httpServer.listen(3000, () => {
   log.info('Server is running on port 3000')
   startSocket()
+  app.get('/ip', (request, response) => response.send(request.ip))
   routes(app)
   swaggerDocs(app)
 
