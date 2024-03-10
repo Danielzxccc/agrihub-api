6f1bbacd686dfb8e1ba347ff4a96df106308552a
Danielzxccc <araojodaniel321@gmail.com>
2024-03-10 12:10:08 +0800

    trust proxy v4

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app: Express = express()
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-// app.set('trust proxy', 1)
+app.set('trust proxy', 1)
 
 app.use(session(sessionConfig))
 
