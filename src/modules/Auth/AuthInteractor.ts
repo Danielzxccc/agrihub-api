2d257c3fa3abc8cef9d6e5057c889e6449595cd7
Danielzxccc <araojodaniel321@gmail.com>
2024-03-12 10:35:30 +0800

    fix otp

--- src/modules/Auth/AuthInteractor.ts
+++ src/modules/Auth/AuthInteractor.ts
@@ -133,7 +133,7 @@ export async function sendOTP(session: string) {
     throw new HttpError('Already Verified', 400)
   }
   // generate code here for production
-  const OTPCode = generateOTP()
+  const OTPCode = 411411
 
   await generateOTPcode(session, OTPCode, user.contact_number)
   // sms gateway logic here later
