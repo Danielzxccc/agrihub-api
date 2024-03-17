407f0c0a497a560a41c1777292df0d6b0ba8911a
Xaalf104 <alsharief.dinglasa06@gmail.com>
2024-03-17 09:51:40 +0800

    added accessguard

--- src/modules/Privacy Policy/PrivacyPolicyRoute.ts
+++ src/modules/Privacy Policy/PrivacyPolicyRoute.ts
@@ -1,14 +1,15 @@
 import express from 'express'
 import * as PrivacyPolicyController from './PrivacyPolicyController'
-import { UserGuard } from '../AuthGuard/UserGuard'
+import { AccessGuard, UserGuard } from '../AuthGuard/UserGuard'
 
 export const PrivacyPolicyRouter = express.Router()
 
 //Privacy Policy
-PrivacyPolicyRouter.get('/details', PrivacyPolicyController.listPrivacyPolicy)
+PrivacyPolicyRouter.get('/', PrivacyPolicyController.listPrivacyPolicy) //Landing page of privacy policy
 
 PrivacyPolicyRouter.put(
   '/update',
+  AccessGuard('privacy_policy'),
   UserGuard(['admin', 'asst_admin']),
   PrivacyPolicyController.updatePrivacyPolicy
 )
