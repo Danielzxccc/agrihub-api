2d1848f81ef254e1aad5b9da229fe20bc29d3c3e
Xaalf104 <alsharief.dinglasa06@gmail.com>
2024-03-19 11:39:38 +0800

    Terms and Conditions CMS

--- draft_sql/tnc.sql
+++ draft_sql/tnc.sql
@@ -1,4 +1,4 @@
-CREATE TABLE privacy_policy(
+CREATE TABLE terms_condition(
     id SERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     createdAt timestamp DEFAULT CURRENT_TIMESTAMP,
---
+++ src/modules/Terms&Conditions/TermsConditionsController.ts
@@ -0,0 +1,31 @@
+import errorHandler from '../../utils/httpErrorHandler'
+import { SessionRequest } from '../../types/AuthType'
+import { Response } from 'express'
+import zParse from '../../utils/zParse'
+import * as Interactor from './TermsConditionsInteractor'
+import * as Schema from '../../schema/TermsConditions'
+
+export async function listTermsConditions(req: SessionRequest, res: Response) {
+  try {
+    const contents = await Interactor.listTermsConditions()
+    res.status(200).json(contents)
+  } catch (error) {
+    errorHandler(res, error)
+  }
+}
+
+export async function updateTermsConditions(
+  req: SessionRequest,
+  res: Response
+) {
+  try {
+    const { body } = await zParse(Schema.UpdateTermsConditions, req)
+
+    const updatedTermsConditions = await Interactor.updateTermsConditions(body)
+    res
+      .status(200)
+      .json({ message: 'Updated Successfully', data: updatedTermsConditions })
+  } catch (error) {
+    errorHandler(res, error)
+  }
+}
