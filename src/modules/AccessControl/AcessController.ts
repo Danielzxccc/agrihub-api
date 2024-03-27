b8f1f0fcfb4264f17d75c2042d47d90cf3168ebd
anotherinthefire <rongodfreyultra@gmail.com>
2024-03-27 09:56:25 +0800

    feat: access control audit logs

--- src/modules/AccessControl/AcessController.ts
+++ src/modules/AccessControl/AcessController.ts
@@ -4,13 +4,20 @@ import * as Interactor from './AccessInteractor'
 import * as Schema from '../../schema/AccessControlSchema'
 import zParse from '../../utils/zParse'
 import { SessionRequest } from '../../types/AuthType'
+import { createAuditLog } from '../AuditLogs/AuditLogsInteractor'
 
-export async function createNewAdmin(req: Request, res: Response) {
+export async function createNewAdmin(req: SessionRequest, res: Response) {
   try {
     const { body } = await zParse(Schema.NewAdmin, req)
     const { email, password, access } = body
 
     const data = await Interactor.createNewAdmin({ email, password }, access)
+
+    await createAuditLog({
+      action: 'Created New Admin',
+      section: 'Admin Management',
+      userid: req.session.userid,
+    })
 
     res.status(201).json({ message: 'Created Successfully', data })
   } catch (error) {
@@ -24,6 +31,12 @@ export async function updateAdminAccess(req: SessionRequest, res: Response) {
     const { id } = req.params
 
     await Interactor.updateAdminAccess(id, body)
+
+     await createAuditLog({
+      action: 'Updated Admin Access',
+      section: 'Admin Management',
+      userid: req.session.userid,
+    })
 
     res.status(200).json({ message: 'Updated Successfully' })
   } catch (error) {
