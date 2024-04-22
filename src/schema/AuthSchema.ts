e95e14009de02676e5689d3a901aabf5da086b4d
Danielzxccc <araojodaniel321@gmail.com>
2024-04-22 14:57:03 +0800

    chore: add middlename

--- src/schema/AuthSchema.ts
+++ src/schema/AuthSchema.ts
@@ -46,6 +46,8 @@ import { z } from 'zod'
  *       properties:
  *         firstname:
  *           type: string
+ *         middlename:
+ *           type: string
  *         lastname:
  *           type: string
  *         birthdate:
@@ -263,6 +265,7 @@ export const verifyLevelOne = z.object({
 export const verifyLevelTwo = z.object({
   body: z.object({
     firstname: z.string({ required_error: 'firstname is required' }),
+    middlename: z.string().optional(),
     lastname: z.string({ required_error: 'lastname is required' }),
     birthdate: z.string({ required_error: 'birthdate is required' }),
     present_address: z.string({
