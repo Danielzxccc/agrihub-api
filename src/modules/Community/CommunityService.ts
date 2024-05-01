adc180d504238fa9b2e0655f78cab88611b20a14
Danielzxccc <araojodaniel321@gmail.com>
2024-05-01 14:24:16 +0800

    fix: crop history pagination

--- src/modules/Community/CommunityService.ts
+++ src/modules/Community/CommunityService.ts
@@ -381,7 +381,12 @@ export async function getTotalPlantedReports({
   }
 
   if (previous_id) {
-    query = query.where('last_harvest_id', '=', previous_id)
+    query = query.where((eb) =>
+      eb.or([
+        eb('ccr.last_harvest_id', '=', previous_id),
+        eb('ccr.id', '=', previous_id),
+      ])
+    )
   }
 
   if (month.length) {
